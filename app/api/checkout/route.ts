29eeeba31fc0cc7754172ddb65a59c823f020075
Lucas Rocha <lucasrocha.dv@gmail.com>
2026-09-07 22:36:50 -0300

    Adicionando métodos de pagamentos do stripe

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -36,6 +36,7 @@ export async function POST(req: NextRequest) {
 
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
+      payment_method_types: ["card", "pix", "boleto"],
       line_items: [
         {
           price_data: {
