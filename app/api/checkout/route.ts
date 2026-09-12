e97c6baaafd21435c3ce277d938bc8ecd4b92baa
Lucas Rocha <lucasrocha.dv@gmail.com>
2026-09-12 02:18:08 -0300

    Remove pix como método de pagamento

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -36,7 +36,7 @@ export async function POST(req: NextRequest) {
 
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
-      payment_method_types: ["card", "pix", "boleto"],
+      payment_method_types: ["card", "boleto"],
       line_items: [
         {
           price_data: {
